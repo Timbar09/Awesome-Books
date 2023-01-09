4f5e5f8c31077e68b5e6957e3807f0f5f3eaba11
mohi <mohisadeghi302@gmail.com>
2023-01-09 21:17:46 +0330

    Fix some bug

--- app/js/utilities.js
+++ app/js/utilities.js
@@ -5,28 +5,34 @@ export let collection = [
   },
 ];
 export let objCounter = 1;
+let rmCnt = 0;
 export let cancelButtons = document.querySelectorAll(".hero__book-remove");
 export const addBtn = document.querySelector(".book-add");
 let booksContainer = document.querySelector(".hero__collection");
 export let bookContainers = document.querySelectorAll(".hero__book");
 
 export const addBook = () => {
+  objCounter = objCounter + 1;
   const title = document.querySelector(".book-title").value;
   const author = document.querySelector(".book-author").value;
   collection.push({
     bookTitle: title,
     bookAuthor: author,
   });
+  console.log(collection[objCounter - 1]);
   localStorage.setItem("book", JSON.stringify(collection));
-  objCounter = objCounter + 1;
   return;
 };
 
 export const insertBook = (objCnt) => {
   booksContainer.innerHTML += `
         <li class="hero__book" id=${objCnt}>
-          <p class="hero__book-title">${collection[objCnt - 1].bookTitle}</p>
-          <p class="hero__book-author">${collection[objCnt - 1].bookAuthor}</p>
+          <p class="hero__book-title">${
+            collection[objCnt - 1 - rmCnt].bookTitle
+          }</p>
+          <p class="hero__book-author">${
+            collection[objCnt - 1 - rmCnt].bookAuthor
+          }</p>
           <button class="hero__book-remove" id=${objCnt}>Remove</button>
           <hr />
         </li>
@@ -36,9 +42,22 @@ export const insertBook = (objCnt) => {
   return;
 };
 
+export const removeLoad = () => {
+  cancelButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      collection.splice(btn.id - 1, 1);
+      bookContainers[btn.id - 1].classList.add("close");
+      rmCnt = rmCnt + 1;
+      console.log(btn);
+    });
+  });
+};
+
 const data = () => {
-  collection = JSON.parse(localStorage.getItem("book"));
-  for (let i = 2; i < collection.length; i++) insertBook(i);
+  //   collection = JSON.parse(localStorage.getItem("book"));
+  if (collection) {
+    for (let i = 2; i < collection.length; i++) insertBook(i);
+  }
 };
 
 window.addEventListener("DOMContentLoaded", data);
