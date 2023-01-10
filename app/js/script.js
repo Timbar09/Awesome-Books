10e4583ea48cc89345e74b34545367e38c66994e
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-01-10 11:31:28 +0200

    Add Book class

--- app/js/script.js
+++
@@ -1,9 +0,0 @@
-import { addBook, insertBook, removeLoad } from './utilities.js';
-
-const addBtn = document.querySelector('.book-add');
-
-addBtn.addEventListener('click', () => {
-  addBook();
-  insertBook();
-  removeLoad();
-});
