1e9e6c19571d4b2a433bfc83701fab80a19391de
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-01-11 12:55:52 +0200

    Resolve linters

--- app/js/user.js
+++ app/js/user.js
@@ -6,18 +6,15 @@ const bookListSection = document.querySelector('#books-list');
 navLinks.forEach((link, index) => {
   link.addEventListener('click', () => {
     if (index === 0) {
-      if (bookListSection.classList.contains('close'))
-        bookListSection.classList.remove('close');
+      if (bookListSection.classList.contains('close')) { bookListSection.classList.remove('close'); }
       addBookSection.classList.add('close');
       contactSection.classList.add('close');
     } else if (index === 1) {
-      if (addBookSection.classList.contains('close'))
-        addBookSection.classList.remove('close');
+      if (addBookSection.classList.contains('close')) { addBookSection.classList.remove('close'); }
       bookListSection.classList.add('close');
       contactSection.classList.add('close');
     } else if (index === 2) {
-      if (contactSection.classList.contains('close'))
-        contactSection.classList.remove('close');
+      if (contactSection.classList.contains('close')) { contactSection.classList.remove('close'); }
       bookListSection.classList.add('close');
       addBookSection.classList.add('close');
     }
